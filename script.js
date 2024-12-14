27db17c516ca55cf149dcaa33805a81b0c81213e
PepeOnGithub <106378846+PepeOnGithub@users.noreply.github.com>
2024-12-14 16:06:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-function launchGame() {
-  window.location.href = "minecraft://";
+function launchMinecraft() {
+    window.location.href = "intent:#Intent;action=android.intent.action.MAIN;category=android.intent.category.LAUNCHER;package=com.mojang.minecraftpe;end";
 }
