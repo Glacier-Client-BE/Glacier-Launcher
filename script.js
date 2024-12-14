505a05db111fb93e111a431f75f68ea8b77df55e
PepeOnGithub <106378846+PepeOnGithub@users.noreply.github.com>
2024-12-14 15:19:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
 document.getElementById('launch-game').addEventListener('click', function() {
-  const userAgent = navigator.userAgent || navigator.vendor;
-  if (/android/i.test(userAgent)) {
-    window.location.href = 'intent://#Intent;scheme=minecraft;package=com.mojang.minecraftpe;end;';
-  } else {
-    window.location.href = 'minecraft://';
-  }
+  window.location.href = 'intent://#Intent;scheme=minecraft;package=com.mojang.minecraftpe;end;';
 });
